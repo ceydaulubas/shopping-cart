73d007675adeb0ff0b88246eedf9cf04ab120a95
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2023-03-19 01:15:25 +0300

    Offcanvas created for shopping cart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, ReactNode, useState } from "react";
-
+import { ShoppingCart } from "../components/ShoppingCart";
 type ShoppingCartProviderProps = {
   children: ReactNode;
 };
@@ -10,10 +10,14 @@ type CartItem = {
 };
 
 type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
   getItemQuantatiy: (id: number) => number;
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
 };
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
@@ -23,7 +27,13 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false);
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0);
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
 
   function getItemQuantatiy(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -67,8 +77,11 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   }
 
   return (
-    <ShoppingCartContext.Provider value={{ getItemQuantatiy, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
+    <ShoppingCartContext.Provider
+      value={{ getItemQuantatiy, increaseCartQuantity, decreaseCartQuantity, removeFromCart, openCart, closeCart, cartItems, cartQuantity }}
+    >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
